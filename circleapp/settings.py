072a94b84d5cbc580aed9c66621b4080c00dca24
Koji Kato <happiitaro@gmail.com>
2026-03-03 12:36:50 +0900

    fix allowed hosts

--- circleapp/settings.py
+++ circleapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ljc_q%xomoh-*s+b_gwr)(9@e!!l@^@0k+xy+0ox5+tm@synk*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["circleapp-django.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
